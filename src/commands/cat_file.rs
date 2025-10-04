ccf1ec2b77b7022bbeec5091759e3cbf5fa190ca
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-10-04 16:01:00 +0530

    read tree object

---
+++ src/commands/cat_file.rs
@@ -0,0 +1,6 @@
+use anyhow::Result;
+
+pub fn invoke(pretty_print: bool, object_hash: &str) -> Result<()> {
+    // your cat-file logic here
+    Ok(())
+}
---
+++ src/commands/hash_object.rs
@@ -0,0 +1,6 @@
+use anyhow::Result;
+
+pub fn invoke(write: bool, file: &str) -> Result<()> {
+    // your hash-object logic here
+    Ok(())
+}
